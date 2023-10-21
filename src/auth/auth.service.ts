bedbc4242a4aec0339040e67b6f17037f2d4f6bc
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-10-21 21:16:15 +0300

    learning Nestjs

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import { ConfigService } from "@nestjs/config";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService
+  ) {}
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException("Credential taken");
+        }
+        throw error;
+      } else {
+        throw error;
+      }
+    }
+  }
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const isMatches = await argon.verify(user.hash, dto.password);
+    if (!isMatches) {
+      throw new UnauthorizedException();
+    }
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string
+  ): Promise<{ access_token: string }> {
+    const payload = { userId, email };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: "15m",
+      secret: this.config.get("SECRET"),
+    });
+
+    return { access_token: token };
+  }
+}
